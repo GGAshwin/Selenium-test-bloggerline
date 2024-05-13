2352cdb1725b0a08afb14189c8220386628977e7
GGAshwin <ashwinprabhu2001@gmail.com>
2024-05-13 21:01:50 +0530

    added logout

--- main..py
+++ main..py
@@ -9,13 +9,14 @@ import time
 service = Service(executable_path='./chromedriver.exe')
 driver = webdriver.Chrome(service=service)
 
+
 def login_user():
     driver.maximize_window()
     driver.get("http://localhost:3000/")
     assert "Bloggerline" in driver.title
     time.sleep(2)
     elem = driver.find_element(
-    By.XPATH, '//*[@id="root"]/div[1]/div[3]/ul/li[1]/a')
+        By.XPATH, '//*[@id="root"]/div[1]/div[3]/ul/li[1]/a')
     print(elem)
     elem.click()
     username = driver.find_element(
@@ -30,24 +31,35 @@ def login_user():
     assert "No results found." not in driver.page_source
     # driver.close()
 
+
 def write_post_and_delete():
-    elem = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/ul/li[2]/a')
+    elem = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[1]/div[2]/ul/li[2]/a')
     elem.click()
     time.sleep(2)
-    tag_filed = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/form/div[1]/div/div[2]/input')
+    tag_filed = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[2]/form/div[1]/div/div[2]/input')
     tag_filed.send_keys('TestTag')
     tag_filed.send_keys(Keys.RETURN)
-    title_filed = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/form/div[1]/input[2]')
+    title_filed = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[2]/form/div[1]/input[2]')
     title_filed.send_keys("Test title from Selenium")
-    story_text = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/form/div[2]/textarea')
+    story_text = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[2]/form/div[2]/textarea')
     story_text.send_keys("This is a test post made with selenium web driver")
     submit_button = driver.find_element(By.CLASS_NAME, 'writeSubmit')
     submit_button.click()
     time.sleep(3)
-    delete_post = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div[1]/h1/div/i[2]')
+    delete_post = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[2]/div/div[1]/h1/div/i[2]')
     delete_post.click()
-    time.sleep(5)
+    time.sleep(2)
+    logout_button = driver.find_element(
+        By.XPATH, '//*[@id="root"]/div[1]/div[2]/ul/li[3]')
+    logout_button.click()
+    time.sleep(2)
     driver.close()
+
 
 # Run tests
 if __name__ == "__main__":
